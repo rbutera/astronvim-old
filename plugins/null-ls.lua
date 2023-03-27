219ff6148574895e43be5598f1b54993e64adfbd
Rai Butera <rai@rbutera.com>
2023-03-27 16:53:42 +0100

    port plugins from astronvim v2

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,7 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,6 +11,29 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.eslint_d,
+      null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.diagnostics.luacheck,
+      null_ls.builtins.diagnostics.mypy,
+      null_ls.builtins.code_actions.shellcheck,
+      null_ls.builtins.completion.tags,
+      -- null_ls.builtins.formatting.markdownlint,
+      null_ls.builtins.formatting.prettier,
+      -- null_ls.builtins.formatting.terrafmt,
+      null_ls.builtins.formatting.terraform_fmt,
+      null_ls.builtins.formatting.gofumpt,
+      null_ls.builtins.formatting.shfmt,
+      null_ls.builtins.diagnostics.tsc,
+      -- null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.isort,
+      null_ls.builtins.formatting.stylua,
+      -- null_ls.builtins.diagnostics.pylint,
+      -- golang
+      null_ls.builtins.formatting.gofumpt,
+      null_ls.builtins.formatting.goimports,
+      null_ls.builtins.diagnostics.revive,
     }
     return config -- return final config table
   end,
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,228 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      opts.section.header.val = {
+        " ",
+        " ",
+        " ",
+        " ",
+        " ██████  ██      ██   ██ ███████",
+        "██       ██      ██   ██ ██    ",
+        "██   ███ ██      ███████ █████ ",
+        "██    ██ ██      ██   ██ ██    ",
+        " ██████  ███████ ██   ██ ██    ",
+        " ",
+        " ",
+        " ",
+      }
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function() require("catppuccin").setup {} end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = false,
+  },
+  {
+    "axelvc/template-string.nvim",
+    lazy = false,
+    config = function()
+      require("template-string").setup {
+        filetypes = {
+          "typescript",
+          "javascript",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+          "vue",
+        },
+        jsx_brackets = true,
+      }
+    end,
+  },
+  { "j-hui/fidget.nvim", lazy = false },
+  { "VonHeikemen/searchbox.nvim", lazy = false },
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = false,
+  },
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    requires = "nvim-lua/plenary.nvim",
+    config = function() require("todo-comments").setup {} end,
+  },
+  { "echasnovski/mini.nvim", branch = "stable", lazy = false },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function() require("trouble").setup {} end,
+  },
+  {
+    "folke/lsp-colors.nvim",
+    lazy = false,
+  },
+  {
+    "ggandor/leap.nvim",
+    lazy = false,
+    requires = "tpope/vim-repeat",
+    config = function()
+      local l = require "leap"
+      l.set_default_keymaps()
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    lazy = false,
+    config = function() require("nvim-surround").setup {} end,
+  },
+  {
+    "kosayoda/nvim-lightbulb",
+    lazy = false,
+    requires = "antoinemadec/FixCursorHold.nvim",
+    config = function() require("nvim-lightbulb").setup { autocmd = { enabled = true } } end,
+  },
+  {
+    "windwp/nvim-spectre",
+    lazy = false,
+    requires = "windwp/nvim-spectre",
+  },
+  {
+    "bkad/CamelCaseMotion",
+    lazy = false,
+    config = function() vim.api.nvim_set_var("camelcasemotion_key", "<leader>") end,
+  },
+  {
+    "hashivim/vim-terraform",
+  },
+  {
+    "tpope/vim-eunuch",
+    lazy = false,
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+    config = function()
+      -- vim.o.foldcolumn = "0"
+      -- vim.o.foldnestmax = 3
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+      local ftMap = {
+        vim = "indent",
+        python = { "indent" },
+        git = "",
+      }
+
+      local function customizeSelector(bufnr)
+        local function handleFallbackException(err, providerName)
+          if type(err) == "string" and err:match "UfoFallbackException" then
+            return require("ufo").getFolds(providerName, bufnr)
+          else
+            return require("promise").reject(err)
+          end
+        end
+
+        return require("ufo")
+          .getFolds("lsp", bufnr)
+          :catch(function(err) return handleFallbackException(err, "treesitter") end)
+          :catch(function(err) return handleFallbackException(err, "indent") end)
+      end
+
+      require("ufo").setup {
+        provider_selector = function(bufnr, filetype, buftype) return ftMap[filetype] or customizeSelector end,
+      }
+    end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function() require("Comment").setup() end,
+  },
+  {
+    "nat-418/boole.nvim",
+    lazy = false,
+    command = "Boole",
+    config = function()
+      require("boole").setup {
+        mappings = {
+          increment = "<C-a>",
+          decrement = "<C-x>",
+        },
+      }
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        panel = {
+          enabled = false,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = false,
+          auto_trigger = false,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = "node", -- Node.js version must be > 16.x
+        server_opts_overrides = {},
+      }
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    lazy = false,
+    after = { "copilot.lua" },
+    config = function() require("copilot_cmp").setup() end,
+  },
 }
