a07d779e051a646d636c590374ee988778ffeafc
Rai Butera <rai@rbutera.com>
2024-04-18 11:34:02 +0100

    add leetcode

--- plugins/user.lua
+++ plugins/user.lua
@@ -233,4 +233,22 @@ return {
     after = { "copilot.lua" },
     config = function() require("copilot_cmp").setup() end,
   },
+  {
+   "kawre/leetcode.nvim",
+    lazy = false,
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        -- configuration goes here
+    },
+  }
 }
